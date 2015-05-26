6779ddacf9320b3f08498d568500cce6cdf23b84
Tienlong Pham <aglorei88@gmail.com>
2015-05-25 18:24:51 -0700

    updated

---
+++ client/queue.js
@@ -0,0 +1,37 @@
+// queue item
+var queueNode = function(item) {
+	this.item = item;
+	this.next = null;
+};
+
+// Queue class. enqueue to last, dequeue from first
+var Queue = function() {
+	var first = null;
+	var last = null;
+
+	this.enqueue = function(item) {
+		if (!first) {
+			last = new queueNode(item);
+			first = last;
+		} else {
+			last.next = new queueNode(item);
+			last = last.next;
+		}
+	};
+
+	this.dequeue = function() {
+		if (first) {
+			var item = first.item;
+
+			first = first.next;
+			if (!first) { last = null; } // if empty, empty queue
+
+			return item;
+		}
+		return null;
+	};
+
+	this.isEmpty = function() {
+		return (!first);
+	};
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-## Construct paths from an origin word to a target word where intermediate words differ by a character from their adjacent words.
+## Construct paths from an origin word to a target word where intermediate words differ by a character from their adjacent words. Based on Lewis Carroll's [http://en.wikipedia.org/wiki/Word_ladder](Word Ladder) game.
 
 ### Find me at [https://pathsearch.herokuapp.com/](https://pathsearch.herokuapp.com/)
